f62750212781034a1e175c932e39ad4b4eb8ccdf
TaeJoon Park(Dell Inspiron7577) <alfj23@naver.com>
2018-11-05 13:44:44 +0900

    P.O.W Idle State 완성
    포로 대기 상태 좌우 이동하도록 구현함

--- Project/Slug Defense/pow.py
+++ Project/Slug Defense/pow.py
@@ -5,15 +5,15 @@ import game_world
 
 # POW Speed
 
-PIXEL_PER_METER = (10.0 / 0.3)
-RUN_SPEED_KMPH = 10.0
+PIXEL_PER_METER = (10.0 / 0.15)
+RUN_SPEED_KMPH = 0.01
 RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
 RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
 RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
 
 # POW Speed
 
-TIME_PER_ACTION = 0.5  # 액션 당 시간
+TIME_PER_ACTION = 1.0 # 액션 당 시간
 ACTION_PER_TIME = 1.0  # 액션 마다 달라서 따로 빼놓음?
 FRAMES_PER_ACTION = 13
 
@@ -35,57 +35,69 @@ key_event_table = {
 class IdleState:
 
     @staticmethod
-    def enter(player, event):
+    def enter(prisoner, event):
         pass
 
     @staticmethod
-    def exit(player, event):
+    def exit(prisoner, event):
         pass
 
     @staticmethod
-    def do(player):
-        pass
+    def do(prisoner):
+        if prisoner.dir == 1:
+            prisoner.velocity += RUN_SPEED_PPS
+            if prisoner.x >= 180 - 1:
+                prisoner.velocity = -1
+                prisoner.dir = -1
+        elif prisoner.dir == -1:
+            prisoner.velocity -= RUN_SPEED_PPS
+            if prisoner.x <= 20 + 1:
+                prisoner.velocity = 1
+                prisoner.dir = 1
+        prisoner.frame = (prisoner.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 13
 
-
-
-    @staticmethod
-    def draw(player):
-        pass
-
-class DriveState:
-
-    @staticmethod
-    def enter(player, event):
-        pass
-
-    @staticmethod
-    def exit(player, event):
-        pass
-
-    @staticmethod
-    def do(player):
+        prisoner.x += prisoner.velocity * game_framework.frame_time
+        prisoner.x = clamp(0 + 20, prisoner.x, 200 - 20)
+        print(prisoner.velocity)
         pass
 
     @staticmethod
-    def draw(player):
+    def draw(prisoner):
+        if prisoner.dir == 1:
+            prisoner.image.clip_draw(int(prisoner.frame) * 50, 0, 50, 50, prisoner.x, prisoner.y)
+        else:
+            prisoner.image.clip_draw(int(prisoner.frame) * 50, 50, 50, 50, prisoner.x, prisoner.y)
+        pass
+
+
+class WatchState:
+
+    @staticmethod
+    def enter(prisoner, event):
+        pass
+
+    @staticmethod
+    def exit(prisoner, event):
+        pass
+
+    @staticmethod
+    def do(prisoner):
+        pass
+
+    @staticmethod
+    def draw(prisoner):
         pass
 
 
 next_state_table = {
-    IdleState: {RIGHT_UP: DriveState, LEFT_UP: DriveState,
-                RIGHT_DOWN: DriveState, LEFT_DOWN: DriveState,
-                X: IdleState
-               },
-    DriveState: {RIGHT_UP: IdleState, LEFT_UP: IdleState,
-                 LEFT_DOWN: IdleState, RIGHT_DOWN: IdleState,
-                 X: DriveState
-               }
+    IdleState: {},
+    WatchState: {}
 }
 
 
 class Pow:
     def __init__(self):
-        self.x, self.y = 1600 // 2, 70
+        self.x, self.y = 100, 60
         self.image = load_image('P_O_W.png')
         self.dir = 1
         self.velocity = 0
