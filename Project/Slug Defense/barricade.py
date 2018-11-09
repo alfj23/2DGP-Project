52419280b4da430605ce60121c827d07af223987
TaeJoon Park(Dell Inspiron7577) <alfj23@naver.com>
2018-11-09 11:59:04 +0900

    barricade 구현
    생성자 xy좌표 = 200, 240, 체력 = 300

--- Project/Slug Defense/barricade.py
+++ Project/Slug Defense/barricade.py
@@ -7,6 +7,9 @@ class Barricade:
     image = None
 
     def __init__(self, x=400, y=300, velocity=3):
+        self.x, self.y = 200, 40 + 200
+        self.image = load_image('barricade.png')
+        self.hp = 300
         pass
 
     def update(self):
@@ -14,6 +17,6 @@ class Barricade:
 
     def get_bb(self):
         pass
-    
+
     def draw(self):
         pass
