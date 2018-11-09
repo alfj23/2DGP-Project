7c4a8fdcdf370dd1af17085af97e7edfd8139d16
TaeJoon Park(Dell Inspiron7577) <alfj23@naver.com>
2018-11-09 12:08:04 +0900

    barricade 구현
    체력 수치 렌더링 완료, droptank_bomb 과 충돌시 체력 감소하는 것 확인가능

--- Project/Slug Defense/barricade.py
+++ Project/Slug Defense/barricade.py
@@ -9,6 +9,7 @@ class Barricade:
     def __init__(self, x=400, y=300, velocity=3):
         self.x, self.y = 200, 40 + 200
         self.image = load_image('barricade.png')
+        self.font = load_font('ENCR10B.TTF', 16)
         self.hp = 300
         pass
 
@@ -22,4 +23,6 @@ class Barricade:
     def draw(self):
         self.image.draw(self.x, self.y)
         draw_rectangle(*self.get_bb())
+        self.font.draw(self.x - 60, self.y + 50,
+                       '(HP : %i)' % self.hp, (255, 0, 0))
         pass
