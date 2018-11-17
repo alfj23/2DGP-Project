c9ef5009aafd38c31964578407f8e1cc976af08d
TaeJoon Park(Dell Inspiron7577) <alfj23@naver.com>
2018-11-18 00:00:56 +0900

    pause menu 구현 완료
    x : 프로그램 종료, t : 타이틀 화면으로 이동
    두 기능 구현완료 폰트로만 메뉴 표현을 해놔서 조금 엉성해 보임..

--- Project/Slug Defense/mygame.py
+++ Project/Slug Defense/mygame.py
@@ -2,7 +2,8 @@ import game_framework
 import pico2d
 import start_state
 import main_state
+import pause_state
 
 pico2d.open_canvas(800, 600)
-game_framework.run(main_state)
+game_framework.run(pause_state)
 pico2d.close_canvas()
--- Project/Slug Defense/pause_state.py
+++ Project/Slug Defense/pause_state.py
@@ -2,19 +2,23 @@ from pico2d import *
 import game_framework
 import game_world
 import main_state
+import title_state
 
 __name__ = "pause_state"
 
 class Pause_menu:
     def __init__(self):
         self.image = load_image('pause_menu.png')
-        self.font = load_font('ENCR10B.TTF', 30)
+        self.font = load_font('ENCR10B.TTF', 18)
 
     def update(self):
         pass
 
     def draw(self):
         self.image.draw(800//2, 600//2)
+        self.font.draw(380, 385, 'MENU', (0, 0, 0))
+        self.font.draw(330, 320, 'X: Exit Game', (255, 255, 255))
+        self.font.draw(310, 250, 'T: Back to title', (255, 255, 255))
 
 pause_menu = None
 
@@ -48,6 +52,10 @@ def handle_events():
             game_framework.quit()
         elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
             game_framework.pop_state()
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_x):
+            game_framework.quit()
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_t):
+            game_framework.change_state(title_state)
 
 
 def pause():
