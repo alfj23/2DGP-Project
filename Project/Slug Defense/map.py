57e76d48179e75edb64f54a506f9cfde937ee23e
TaeJoon Park(Dell Inspiron7577) <alfj23@naver.com>
2018-11-17 23:39:59 +0900

    맵에 바운딩 박스 추가
    추후 작업 할 스킬, droptank_bomb 곡선이동 시  필요하여 바운딩 박스 추가하였음.

--- Project/Slug Defense/map.py
+++ Project/Slug Defense/map.py
@@ -3,13 +3,14 @@ from pico2d import *
 class Map:
     def __init__(self):
         self.image = load_image('maaap.png')
+        self.x, self.y = 0, 200
 
     def update(self):
         pass
-    
+
     def get_bb(self):
-        pass
+        return self.x, self.y, self.x + 799, self.y + 15
 
     def draw(self):
-        self.image.draw_to_origin(0 - 2, 0 + 200 , 1600 * 1.367, 220 * 1.367)
-       # self.image.draw(1200, 30)
+        self.image.draw_to_origin(self.x - 2, self.y, 1600 * 1.367, 220 * 1.367)  # 맵 높이 300으로 늘리기 위해 배율을 1.367로 하였음.
+        draw_rectangle(*self.get_bb())
