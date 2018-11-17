aff85015b4a40420eda85fd40d3adb92a226588d
TaeJoon Park(Dell Inspiron7577) <alfj23@naver.com>
2018-11-17 18:09:03 +0900

    cannon 시간 적용
    ui를 추가했더니 프레임이 떨어져서 포탄들 이동이 들쭉날쭉함
    시간 적용하여 일정한 움직임을 가지도록 하였음

--- Project/Slug Defense/cannon.py
+++ Project/Slug Defense/cannon.py
@@ -1,7 +1,51 @@
 from pico2d import *
 import game_world
 import main_state
+import game_framework
 
+# cannon Speed
+
+PIXEL_PER_METER = (10.0 / 0.15)
+RUN_SPEED_KMPH = 60
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
+# cannnon Speed
+
+TIME_PER_ACTION = 1.5  # 액션 당 시간
+ACTION_PER_TIME = 1.25 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 8
+
+
+class IdleState:
+
+    @staticmethod
+    def enter(cannon, event):
+        cannon.velocity += RUN_SPEED_PPS
+        pass
+
+    @staticmethod
+    def exit(cannon, event):
+        pass
+
+    @staticmethod
+    def do(cannon):  # 사거리 400
+        cannon.x += cannon.velocity * game_framework.frame_time
+        cannon.frame = (cannon.frame + FRAMES_PER_ACTION*ACTION_PER_TIME*game_framework.frame_time) % 4
+
+        for droptank in main_state.droptanks:
+            if main_state.collide(cannon, droptank):
+                droptank.hp -= cannon.damage
+                game_world.remove_object(cannon)
+                break
+        pass
+
+    @staticmethod
+    def draw(cannon):
+        cannon.image.clip_draw(int(cannon.frame) * 40, 0, 40, 30, cannon.x + 30, cannon.y + 10)
+        if cannon.x > 1600 - 20:
+            game_world.remove_object(cannon)  # cannon 이 범위 벗어날 시 반환됨
 
 class Cannon:
     image = None
@@ -12,22 +56,18 @@ class Cannon:
         self.damage = 200
         if Cannon.image == None:
             self.image = load_image('cannon_ball.png')
+        self.event_que = []
+        self.cur_state = IdleState
+        self.cur_state.enter(self, None)
 
     def update(self):
-        self.x += self.velocity
-        self.frame = (self.frame + 1) % 4
+        self.cur_state.do(self)
 
-        for droptank in main_state.droptanks:
-            if main_state.collide(self, droptank):
-                droptank.hp -= self.damage
-                game_world.remove_object(self)
-                break
 
     def get_bb(self):
         return self.x + 10, self.y, self.x + 45, self.y + 20
 
     def draw(self):
-        self.image.clip_draw(self.frame * 40, 0, 40, 30, self.x + 30, self.y + 10)
+        self.cur_state.draw(self)
         draw_rectangle(*self.get_bb())
-        if self.x > 1600 - 20:
-            game_world.remove_object(self)  # cannon 이 범위 벗어날 시 반환됨
+
--- Project/Slug Defense/droptank_bomb.py
+++ Project/Slug Defense/droptank_bomb.py
@@ -1,81 +1,50 @@
 from pico2d import *
 import game_world
 import main_state
+import game_framework
 
-# barricade Events
-REPAIR, UNDER50, DESTROYED = range(3)  # repair barricade / hp_amount under 50% / barricade destroyed
-# barricade States
+# bomb Speed
+
+PIXEL_PER_METER = (10.0 / 0.15)
+RUN_SPEED_KMPH = 30
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
+# bomb Speed
+
+TIME_PER_ACTION = 1.5  # 액션 당 시간
+ACTION_PER_TIME = 1.25 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 8
+
 class IdleState:
+
     @staticmethod
-    def enter(barricade, event):
+    def enter(cannon, event):
+        cannon.velocity += RUN_SPEED_PPS
         pass
 
     @staticmethod
-    def exit(barricade, event):
+    def exit(cannon, event):
         pass
 
     @staticmethod
-    def do(barricade):
+    def do(cannon):  # 사거리 400
+        cannon.x += cannon.velocity * game_framework.frame_time
+        cannon.frame = (cannon.frame + FRAMES_PER_ACTION*ACTION_PER_TIME*game_framework.frame_time) % 4
+
+        for droptank in main_state.droptanks:
+            if main_state.collide(cannon, droptank):
+                droptank.hp -= cannon.damage
+                game_world.remove_object(cannon)
+                break
         pass
 
     @staticmethod
-    def draw(barricade):
-        pass
-
-
-class HitState:
-    @staticmethod
-    def enter(barricade, event):
-        pass
-
-    @staticmethod
-    def exit(barricade, event):
-        pass
-
-    @staticmethod
-    def do(barricade):
-        pass
-
-    @staticmethod
-    def draw(barricade):
-        pass
-
-
-class DamagedState:
-    @staticmethod
-    def enter(barricade, event):
-        pass
-
-    @staticmethod
-    def exit(barricade, event):
-        pass
-
-    @staticmethod
-    def do(barricade):
-        pass
-
-    @staticmethod
-    def draw(barricade):
-        pass
-
-
-class DestroyedState:
-    @staticmethod
-    def enter(barricade, event):
-        pass
-
-    @staticmethod
-    def exit(barricade, event):
-        pass
-
-    @staticmethod
-    def do(barricade):
-        pass
-
-    @staticmethod
-    def draw(barricade):
-        pass
-
+    def draw(cannon):
+        cannon.image.clip_draw(int(cannon.frame) * 40, 0, 40, 30, cannon.x + 30, cannon.y + 10)
+        if cannon.x > 1600 - 20:
+            game_world.remove_object(cannon)  # cannon 이 범위 벗어날 시 반환됨
 
 class Bomb:
     image = None
@@ -86,8 +55,12 @@ class Bomb:
         self.damage_amount = 100
         if Bomb.image == None:
             self.image = load_image('droptank_bomb.png')
+        self.event_que = []
+        self.cur_state = IdleState
+        self.cur_state.enter(self, None)
 
     def update(self):
+        self.cur_state.do(self)
         self.x -= self.velocity
         self.frame = (self.frame + 1) % 20
 
@@ -105,6 +78,7 @@ class Bomb:
         return self.x - 47, self.y - 7, self.x - 33, self.y + 7
 
     def draw(self):
+        self.cur_state.draw(self)
         self.image.clip_draw(self.frame * 14, 40, 14, 14, self.x-40, self.y)
         draw_rectangle(*self.get_bb())
         if self.x < 0 + 14:
