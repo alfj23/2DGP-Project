a6460de120009a47e31021fd27f60b9c082c9774
TaeJoon Park(Dell Inspiron7577) <alfj23@naver.com>
2018-11-26 21:13:06 +0900

    droptank_BT 구현
    트리는 현재 만들긴 했음.
    상황에 따른 애니메이션을 다르게 출력해야되는데
    이게 제일 문제임

--- Project/Slug Defense/droptank.py
+++ Project/Slug Defense/droptank.py
@@ -1,3 +1,4 @@
+'''
 import game_framework
 from pico2d import *
 import main_state
@@ -201,4 +202,114 @@ class Droptank:
         self.font.draw(self.x - self.bg.window_left - 60, self.y + 50,
                        '(HP : %i)' % self.hp, (255, 0, 0))
         draw_rectangle(*self.get_bb())
+'''
+from pico2d import *
+import main_state
+import random
+from droptank_bomb import Bomb
+import game_framework
+import game_world
+from behavior_tree import BehaviorTree, SelectorNode, SequenceNode, LeafNode
+
+# droptank Speed
+
+PIXEL_PER_METER = (10.0 / 0.4)  # 10 pixel = 30cm
+RUN_SPEED_KMPH = 20
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
+# droptank Speed
+
+TIME_PER_ACTION = 1.25  # 액션 당 시간
+ACTION_PER_TIME = 1.25 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 8
+
+
+class Droptank:
+    def __init__(self):
+        self.x, self.y = random.randint(1600, 3000), 40 + 200
+        self.image = load_image('./resource/droptank/droptank.png')
+        self.velocity = 0
+        self.frame = 0
+        self.hp = 400
+        self.atk_range = 400
+        self.font = load_font('./resource/font/ENCR10B.TTF', 16)
+        self.chk_marking = False
+        self.gold = 200
+        self.build_behavior_tree()
+
+    def build_behavior_tree(self):
+        chk_range_player_node = LeafNode("chk_range_player", self.chk_range_player)
+        chk_range_barricade_node = LeafNode("chk_range_barricade", self.chk_range_barricade)
+        chk_range_prisoner_node = LeafNode("chk_range_prisoner", self.chk_range_prisoner)
+        move_forward_node = LeafNode("move_forward", self.move_forward)
+        fire_bomb_node = LeafNode("fire_bomb", self.fire_bomb)
+        attack_player_node = SequenceNode("attack_player")
+        attack_player_node.add_children(chk_range_player_node, fire_bomb_node)
+        attack_barricade_node = SequenceNode("attack_barricade")
+        attack_barricade_node.add_children(chk_range_barricade_node, fire_bomb_node)
+        attack_prisoner_node = SequenceNode("attack_prisoner")
+        attack_prisoner_node.add_children(chk_range_prisoner_node, fire_bomb_node)
+        attack_node = SelectorNode("attack")
+        attack_node.add_children(attack_player_node, attack_barricade_node, attack_prisoner_node)
+        attack_move_node = SelectorNode("attack_move_node")
+        attack_move_node.add_children(attack_node, move_forward_node)
+        self.bt = BehaviorTree(attack_move_node)
+        pass
+
+    def chk_range_player(self):
+        if self.x - main_state.player.x <= self.atk_range:
+            self.velocity = 0
+            return BehaviorTree.SUCCESS
+        else:
+            return BehaviorTree.FAIL
+        pass
+
+    def chk_range_barricade(self):
+        if self.x - main_state.barricade.x <= self.atk_range:
+            self.velocity = 0
+            return BehaviorTree.SUCCESS
+        else:
+            return BehaviorTree.FAIL
+        pass
+
+    def chk_range_prisoner(self):
+        if self.x - main_state.prisoner.x <= self.atk_range:
+            self.velocity = 0
+            return BehaviorTree.SUCCESS
+        else:
+            return BehaviorTree.FAIL
+        pass
+
+    def move_forward(self):
+        self.velocity = -RUN_SPEED_PPS
+        return BehaviorTree.SUCCESS
+        pass
+
+    def fire_bomb(self):
+        bomb = Bomb(self.x, self.y)
+        bomb.set_background(main_state.map)
+        game_world.add_object(bomb, 1)
+        pass
+
+    def update(self):
+        self.bt.run()
+
+        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME *
+                      game_framework.frame_time) % FRAMES_PER_ACTION
+        self.x += self.velocity * game_framework.frame_time
+        pass
+
+    def draw(self):
+        self.image.clip_draw(int(self.frame) * 100, 0, 100, 80, self.x, self.y)
+        self.font.draw(self.x - self.bg.window_left - 60, self.y + 50,
+                       '(HP : %i)' % self.hp, (255, 0, 0))
+        draw_rectangle(*self.get_bb())
+
+    def get_bb(self):
+        return self.x - self.bg.window_left - 33, self.y - 25, self.x - self.bg.window_left + 32, self.y + 20
+
+    def set_background(self, bg):
+        self.bg = bg
 
