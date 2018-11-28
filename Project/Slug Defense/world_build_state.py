718444e6f9792260dea013e7a961d6b02be89dbc
TaeJoon Park(Dell Inspiron7577) <alfj23@naver.com>
2018-11-28 18:19:57 +0900

    Update world_build_state.py
    월드 빌드 스테이트에 기본 함수들 추가

--- Project/Slug Defense/world_build_state.py
+++ Project/Slug Defense/world_build_state.py
@@ -1,1 +1,25 @@
-pass
+from pico2d import *
+import game_framework
+import game_world
+import main_state
+import json
+
+
+def enter():
+    pass
+
+
+def exit():
+    pass
+
+
+def handle_events():
+    pass
+
+
+def update():
+    pass
+
+
+def draw():
+    pass
